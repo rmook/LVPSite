ab673175bdcf81f3abd304c313978a0589865973
rmook <richard.mook@gmail.com>
2015-03-03 12:36:21 -0500

    completed mobile styling of events section

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 	<!-- ************* FOOTER ************ -->
 
 	<div class="footer">
-		<p>&copy; 2015 Louisville Vocal Project<br>Design by <a href="http://rickmook.com">Rick Mook</a></p>
+		<p>&copy; 2015 Louisville Vocal Project<br><br>Design by <a href="http://rickmook.com">Rick Mook</a></p>
 		<ul class="social-links">
 			<li class="social-icon"><a href="#"><img src="<?php echo BASE_URL;?>img/facebook.png"></a></li>
 			<li class="social-icon"><a href="#"><img src="<?php echo BASE_URL;?>img/email.png"></a></li>
