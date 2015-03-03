2d457b9e1879cb6e35ccb3bebf5eff1c32c06dcc
rmook <richard.mook@gmail.com>
2015-03-03 14:25:32 -0500

    styling events section of desktop view

--- index.php
+++ index.php
@@ -12,50 +12,45 @@ include(ROOT_PATH . 'inc/header.php');
 		<p>Louisville Vocal Project</p>
 	</div>
 
-<!-- GRID SYSTEM -->
-
-	<div class="container-fluid">	
-
 		<!-- first row -->
 
-		<div class="row">
+
 			<!-- ************* EVENTS SECTION ************ -->
 
-			<div class="event col-sm-7">
+			<div class="event">
 				<h1><a href="<?php echo BASE_URL;?>events">Events</a></h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  <a href="#">More Events</a></p>
 				<button>Tickets</button>
 			</div>
 
 			<!-- ************* About SECTION ************ -->
 
-			<div class="splash-about col-sm-5">
-				<h2><a href="<?php echo BASE_URL;?>about">About</a> Louisville Vocal Project</h2>
+			<div class="splash-about">
+				<h1><a href="<?php echo BASE_URL;?>about">About</a> Louisville Vocal Project</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
 			</div>
-		</div>
+
 
 		<!-- second row -->
 
-		<div class="row">
+	
 			<!-- ************* LISTEN SECTION ************ -->
 
-			<div class="listen col-sm-7">
+			<div class="listen">
 				<h1><a href="#">Listen</a></h1>
 				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/193055163&amp;color=8EE59C&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
 			</div>
 
 			<!-- ************* WATCH SECTION ************ -->
 
-			<div class="watch col-sm-5">
+			<div class="watch">
 				<h1><a href="#">Watch</a></h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
 				<div class="screen">
 					<button>Play</button>
 				</div>
 			</div>
-		</div>
-	</div>
+	
 	
 <?php
 include(ROOT_PATH . 'inc/footer.php');
