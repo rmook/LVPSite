1031c12ac37251a11f6d2b43855c9f8924e1f55f
rmook <richard.mook@gmail.com>
2015-03-03 21:57:53 -0500

    added grid structure for main content area

--- index.php
+++ index.php
@@ -13,21 +13,21 @@ include(ROOT_PATH . 'inc/header.php');
 	</div>
 
 		<!-- first row -->
-
+	<div class="grid-container">
 
 			<!-- ************* EVENTS SECTION ************ -->
 
-			<div class="event">
-				<h1><a href="<?php echo BASE_URL;?>events">Events</a></h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  <a href="#">More Events</a></p>
+			<div class="event grid-7 grid-container">
+				<h1 class="grid-4"><a href="<?php echo BASE_URL;?>events">Events</a></h1>
+				<p class="grid-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  <a href="#">More Events</a></p>
 				<button>Tickets</button>
 			</div>
 
 			<!-- ************* About SECTION ************ -->
 
-			<div class="splash-about">
-				<h1><a href="<?php echo BASE_URL;?>about">About</a> Louisville Vocal Project</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
+			<div class="splash-about grid-5 grid-container">
+				<h1 class="grid-4"><a href="<?php echo BASE_URL;?>about">About</a></h1>
+				<p class="grid-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
 			</div>
 
 
@@ -36,21 +36,21 @@ include(ROOT_PATH . 'inc/header.php');
 	
 			<!-- ************* LISTEN SECTION ************ -->
 
-			<div class="listen">
-				<h1><a href="#">Listen</a></h1>
+			<div class="listen grid-7 grid-container">
+				<h1 class="grid-4"><a href="#">Listen</a></h1>
 				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/193055163&amp;color=8EE59C&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
 			</div>
 
 			<!-- ************* WATCH SECTION ************ -->
 
-			<div class="watch">
-				<h1><a href="#">Watch</a></h1>
+			<div class="watch grid-5 grid-container">
+				<h1 class="grid-4"><a href="#">Watch</a></h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
 				<div class="screen">
 					<button>Play</button>
 				</div>
 			</div>
-	
+	</div>
 	
 <?php
 include(ROOT_PATH . 'inc/footer.php');
