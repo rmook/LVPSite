4ce4105c4df7e90fc755713d7aae321ccc159e8d
rmook <richard.mook@gmail.com>
2015-03-04 17:20:12 -0500

    added new files

---
+++ About/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$pageID = 'About';
+require_once("../inc/config.php");
+include(ROOT_PATH . 'inc/header.php');
+
+?>
+
+<!-- ************* BANNER IMAGE ************ -->
+
+	<div class="banner">
+		<p>Louisville Vocal Project</p>
+	</div>
+
+		<!-- first row -->
+	<div class="grid-container">
+
+			<!-- ************* About SECTION ************ -->
+
+			<div class="about grid-12 grid-container">
+				<h1 class="grid-4"><a href="<?php echo BASE_URL;?>About">About</a></h1>
+				<p class="grid-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
+				<p class="grid-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
+				<h2 class="grid-4">Director: Guy Smiley</h2>
+				<div class="headshot"></div>
+				<p class="grid-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat nibh eu lobortis malesuada.</p>
+			</div>
+
+<?php 
+
+include(ROOT_PATH . 'inc/footer.php');
+
+?>
